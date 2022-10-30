81f1f06bea8e8b9070d0562fba2a03304a782a11
Aaron Dewes <aaron.dewes@protonmail.com>
2022-10-30 12:49:12 +0100

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use tempdir::TempDir;
+
 fn main() {
-    println!("Hello, world!");
+    // Create a temporary directory to download the apps to
+    let temp_dir = TempDir::new("gitoxide").unwrap();
+    let temp_dir_path = temp_dir.path();
+    let mut prepare = git_repository::prepare_clone(
+        "https://github.com/AaronDewes/gitoxide-bug-demo",
+        temp_dir_path,
+    ).expect("Failed to prepare");
+    prepare.fetch_only(
+        git_repository::progress::Discard,
+        &std::sync::atomic::AtomicBool::default(),
+    ).expect("Failed to fetch");
+    println!("Downloaded to {}", temp_dir_path.display());
 }
